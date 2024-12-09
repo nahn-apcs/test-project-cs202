1b71a9424d08d1418b4a6e3ddae0cce878850832
nahn-apcs <kenpro030709@gmail.com>
2024-12-09 16:07:33 +0700

    state

---
+++ Include/State.hpp
@@ -0,0 +1,57 @@
+#ifndef STATE_HPP
+#define STATE_HPP
+
+#include <StateID.hpp>
+#include <ResourceID.hpp>
+
+#include <SFML/System/Time.hpp>
+#include <SFML/Window/Event.hpp>
+
+#include <memory>
+
+namespace sf {
+    class RenderWindow;
+}
+
+class StateStack;
+
+class State {
+
+public:
+    typedef std::unique_ptr<State> Ptr;
+
+    struct Context {
+                                    Context(sf::RenderWindow& window, TextureHolder& textures, FontHolder& fonts);
+
+        sf::RenderWindow*           window;
+        TextureHolder*              textures;
+        FontHolder*                 fonts;
+    };
+
+public:
+                                    State(StateStack& stack, Context context);
+    virtual                         ~State();
+    virtual void		            draw() = 0;
+	virtual bool		            update(sf::Time dt) = 0;
+	virtual bool		            handleEvent(const sf::Event& event) = 0;
+
+	virtual void		            onActivate();
+	virtual void		            onDestroy();
+
+protected:
+	void			                requestStackPush(States::ID stateID);
+	void				            requestStackPop();
+	void				            requestStateClear();
+
+	Context				            getContext() const;
+
+
+	private:
+		StateStack*			        mStack;
+		Context				        mContext;
+
+
+
+};
+
+#endif // STATE_HPP
---
+++ Source/State.cpp
@@ -0,0 +1,28 @@
+#include <State.hpp>
+#include <StateStack.hpp>
+
+State::Context::Context(sf::RenderWindow& window, TextureHolder& textures, FontHolder& fonts): window(&window), textures(&textures), fonts(&fonts) {}
+
+State::State(StateStack& stack, Context context): mStack(&stack), mContext(context) {}
+
+State::~State() {}
+
+void State::requestStackPush(States::ID stateID) {
+	mStack->pushState(stateID);
+}
+
+void State::requestStackPop() {
+	mStack->popState();
+}
+
+void State::requestStateClear() {
+	mStack->clearStates();
+}
+
+State::Context State::getContext() const {
+	return mContext;
+}
+
+void State::onActivate(){}
+
+void State::onDestroy() {}
