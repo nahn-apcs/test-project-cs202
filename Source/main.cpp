9062c4835a044ad91d10955fb3ec1deefa93938d
nahn-apcs <kenpro030709@gmail.com>
2024-12-09 14:35:40 +0700

    First commit

---
+++ Source/main.cpp
@@ -0,0 +1,17 @@
+#include <stdexcept>
+#include <iostream>
+
+#include <Application.hpp>
+
+int main () {
+    try {
+        Application app;
+        app.run();
+    } 
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
